968e49b684a1a3b0f23b778c76c3e73e6e108816
EhsanTgv <EhsanTgv@gmail.com>
2021-02-27 20:34:08 +0330

    Implement Animating

--- app/src/main/java/com/taghavi/expand_collapseanimationinrecyclerview/SampleAdapter.kt
+++ app/src/main/java/com/taghavi/expand_collapseanimationinrecyclerview/SampleAdapter.kt
@@ -1,8 +1,11 @@
 package com.taghavi.expand_collapseanimationinrecyclerview
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.taghavi.expand_collapseanimationinrecyclerview.Animating.Companion.getCollapseAnimation
+import com.taghavi.expand_collapseanimationinrecyclerview.Animating.Companion.getExpandAnimation
 import com.taghavi.expand_collapseanimationinrecyclerview.databinding.ItemSampleBinding
 
 class SampleAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -18,11 +21,18 @@ class SampleAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun getItemCount(): Int = 12
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
+        val itemViewHolder = holder as ItemViewHolder
+        with(itemViewHolder.viewBinding) {
+            itemViewHolder.itemView.setOnClickListener {
+                if (image.visibility == View.VISIBLE) {
+                    image.getCollapseAnimation()
+                } else {
+                    image.getExpandAnimation()
+                }
+            }
+        }
     }
 
     class ItemViewHolder(var viewBinding: ItemSampleBinding) :
-        RecyclerView.ViewHolder(viewBinding.root) {
-
-    }
+        RecyclerView.ViewHolder(viewBinding.root)
 }
